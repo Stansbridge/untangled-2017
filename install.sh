a7a0eeb638f1b6985e0fe0ca58c1e89f3e47d316
Oliver Barnwell <barnwelloliver@gmail.com>
2017-06-15 20:06:43 +0100

    Switch to requirements.txt

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # python and pip3
 apt-get -y install python3 python3-pip
-# python dependency packages by name
-pip3 install bson pygame opensimplex
+# python dependency packages
+pip3 install -r requirements.txt
 # pyre dependency package from github
 pip3 install https://github.com/zeromq/pyre/archive/master.zip
